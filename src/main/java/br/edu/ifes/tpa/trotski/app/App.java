dce43cc85ea82ac4a7f4214a7ed6c200976fa0d0
Lucas Possatti <lucas_possatti@hotmail.com>
2015-02-07 14:23:43 -0200

    Indicações de quais dados o programa deve exibir
    TODOs foram adicionados para indicar quais dados devem ser exibidos
    no final da execução.

--- src/main/java/br/edu/ifes/tpa/trotski/app/App.java
+++ src/main/java/br/edu/ifes/tpa/trotski/app/App.java
@@ -12,12 +12,30 @@ public class App {
 		boolean[][] matrizAtivacao = { { false, false, false },
 				{ true, false, false }, { true, false, false } };
 		boolean[][] matrizDesativacao = { { true, true, false },
-				{ false, true, false }, { true, false, true} };
+				{ false, true, false }, { true, false, true } };
 
-		SistemaTransicao sistema = new SistemaTransicao(matrizAtivacao, matrizDesativacao);
-		
+		SistemaTransicao sistema = new SistemaTransicao(matrizAtivacao,
+				matrizDesativacao);
+
+		// Informações sobre o Sistema de Transição.
 		System.out.println(sistema.verificarIrreflexividade());
 
+		// TODO Indicar se as seguintes propriedades são válidas ou não para a
+		// relação de transição: reflexiva; írreflexiva; simétrica;
+		// anti-simétrica; assimétrica; e transitiva.
+		// Se não for, você deve apresentar todos os pares ordenados de estados
+		// presentes ou ausentes que não satisfazem a propriedade.
+
+		// TODO Dizer se a relação é de equivalência ou ordem parcial.
+
+		// TODO Apresentar os fechos reflexivo, simétrico e transitivo da
+		// relação, caso ela não seja reflexiva, simétrica e transitiva,
+		// respectivamente.
+
+		// TODO O sistema é capaz de retornar ao estado inicial?
+
+		// TODO Determinar se é possível para um par de estados (x,y), se é
+		// possível alcançar y partindo de x.
 	}
 
 }
